935fbd0eeea7b4a546d586a5444967a65c0fd51b
Dmitrii <dm161272@gmail.com>
2022-11-03 12:08:22 +0100

    env and database.php edited to access local database

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -7,13 +7,13 @@ use Tests\TestCase;
 class AuthTest extends TestCase
 {
     public function test_login_with_wrong_credentials()
-    {   
+        {   
         $response = $this->post('/api/login', [
             'email' => 'admin@admin.net',
             'password' => '1234567',
         ]);  
         return $response->assertStatus(401);
-    }
+      }
 
     public function test_can_not_create_multiple_admins()
         {   
@@ -26,5 +26,16 @@ class AuthTest extends TestCase
         ]);  
         return $response->assertStatus(409);
         }
-    
+
+    public function test_can_not_create_undeclared_roles()
+        {   
+        $response = $this->post('/api/users', [
+            'name' => 'User_2',
+            'email' => 'user_2@admin.net',
+            'password' => '123456',
+            'password_confirmation' => '123456',
+            'role' => 'not_existed',
+        ]);  
+        return $response->assertStatus(409);
+        } 
 }
