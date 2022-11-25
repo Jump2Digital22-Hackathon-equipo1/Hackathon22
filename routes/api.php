24a16c44aefb9581321fce9e576ddd25faabd9c7
Dmitrii <dm161272@gmail.com>
2022-11-25 17:05:07 +0100

    seeded CSV data in mongoDB

--- routes/api.php
+++ routes/api.php
@@ -31,36 +31,15 @@ Route::get('/index/listallproducts/',[ProductController::class, 'listAllProducts
 
 Route::get('/index/listalldemands/',[DemandController::class, 'listAllDemands']);
 
-Route::get('/index/listallcenters/',[CenterController::class, 'listAllCenters']);
+
 
 
 //PROTECTED ROUTES
 Route::group(['middleware' => ['auth:api']], function() {
 
-// Display following data: Number of companies in each industry, 
-// Number of companies in each size range, 
-// Number of companies in each year of creation
-Route::get('/index/analytics/',[Company::class, 'indexByAnalytics'])
+Route::get('/index/listallcenters/',[CenterController::class, 'listAllCenters'])
 ->middleware('role:admin');
 
-// Display companies' listing ordered by size - ascending or descending.
-Route::get('/index/size/{order}',[Company::class, 'indexBySize'])
-->middleware(['role:admin|user']);
-
-// Display companies' listing ordered by foundation date - ascending or descending.
-Route::get('/index/founded/{order}',[Company::class, 'indexByFounded'])
-->middleware('role:admin|user');
-
-//Logout user
-Route::post('/logout', [UserController::class, 'logout']);
-
-//POST /users : Delete a user.
-Route::post('/users/delete', [UserController::class, 'destroy'])
-->middleware('role:admin');
-
-//GET /users : Get list of users.
-Route::get('/users/list', [UserController::class, 'list'])
-->middleware('role:admin');
 
 }
 );
