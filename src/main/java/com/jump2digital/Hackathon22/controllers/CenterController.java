27eb4b8c1bec8253b8f55d4ff7855b54422a7aa0
rihavior <mmr769@gmail.com>
2022-11-25 14:27:54 +0100

    /get-centersByType/{centerType} added

--- src/main/java/com/jump2digital/Hackathon22/controllers/CenterController.java
+++ src/main/java/com/jump2digital/Hackathon22/controllers/CenterController.java
@@ -1,28 +1,35 @@
 package com.jump2digital.Hackathon22.controllers;
 
+import com.jump2digital.Hackathon22.models.Center;
+import com.jump2digital.Hackathon22.repositories.CenterRepository;
 import com.jump2digital.Hackathon22.services.CenterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.*;
+
+import java.util.List;
 
 @RestController
 public class CenterController {
 
     @Autowired
-    CenterService centerService;
+    CenterRepository centerRepository;
 
-    WebClient client = WebClient.create("http://localhost:8080");
+//    WebClient client = WebClient.create("http://localhost:8080");
+//
+//    UriSpec<RequestBodySpec> uriSpec = client.method(HttpMethod.GET);
+//
+//    RequestBodySpec bodySpec = uriSpec.uri("/centers");
 
-    UriSpec<RequestBodySpec> uriSpec = client.method(HttpMethod.GET);
-
-    RequestBodySpec bodySpec = uriSpec.uri("/centers");
-
-
-
-
-
-
-
+    @GetMapping("/get-centersByType/{centerType}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<Center> getAllATypeCenters(@PathVariable String centerType){
+        return centerRepository.findByCenterType(centerType);
+    }
 }
