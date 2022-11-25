5ff165093b6bc7ba13ba59de436de9599aa43600
Anya <anya.chocolat@gmail.com>
2022-11-25 11:05:48 +0100

    Init

---
+++ src/main/java/com/jump2digital/Hackathon22/Hackathon22Application.java
@@ -0,0 +1,13 @@
+package com.jump2digital.Hackathon22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jump2digital/Hackathon22/Hackathon22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jump2digital.Hackathon22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
